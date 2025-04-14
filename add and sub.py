daab772b7791110127b10b75f48dcad57f6c36ea
sjoecsu <seukmin.joe@csuglobal.edu>
2025-04-14 15:59:27 -0600

    Add files via upload

---
+++ add and sub.py
@@ -0,0 +1,7 @@
+num1 = float(input("Input the first number you'd like to add and subtract: "))
+num2 = float(input("Input the second number you'd like to add and subtract: "))
+
+print("The result of adding the two numbers is:", num1 + num2)
+print("The result of subtracting the two numbers is:", num1 - num2)
+
+input("Press Enter to exit...")
---
+++ mult and div.py
@@ -0,0 +1,7 @@
+num1 = float(input("Input the first number you'd like to multiply and divide: "))
+num2 = float(input("Input the second number you'd like to multiply and divide: "))
+
+print("The result of multiplying the two numbers is", num1 * num2)
+print("The result of dividing the two numbers is", num1 / num2)
+
+input("Press Enter to exit...")
